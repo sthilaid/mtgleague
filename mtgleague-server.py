970480b07ae7b9cd60675882f2bad05ce6cbd414
David St-Hilaire <sthilaire.david@gmail.com>
2018-08-26 22:33:14 -0400

    almost functionnal json serialization
    needs to get rid of the circular ref of DB in Season...

--- mtgleague-server.py
+++ mtgleague-server.py
@@ -2,9 +2,11 @@
 
 from flask import Flask
 from flask import request
-import pickle
+import json
+# import pickle
 from mtgsdk import Card
 import os
+import uuid
 
 app = Flask(__name__)
 
@@ -43,38 +45,93 @@ def admin():
 ###############################################################################
 ## Object Persistence
 
+class JsonSerializable:
+    def valueToJson(self, v):
+        print ("valueToJson(%s)" % v)
+        if getattr(v, 'toJson', False):
+            v = v.toJson()
+        elif isinstance(v, list):
+            v = [self.valueToJson(v_el) for v_el in v]
+        return v
+    
+    def toJson(self):
+        data = {}
+        for k in self.__dict__:
+            v = self.valueToJson(self.__dict__[k])
+            data[k] = v
+        return [type(self).__name__, data]
+
+    @classmethod
+    def jsonToValue(classobj, jsonData):
+        # print ("jsonData(%s)" % jsonData)
+        if isinstance(jsonData, list):
+            if isinstance(jsonData[0], str) and jsonData[0] in globals():
+                valueClassObj = globals()[jsonData[0]]
+                if hasattr(valueClassObj, 'fromJson'):
+                    jsonData = valueClassObj.fromJson(jsonData)
+            else:
+                jsonData = [classobj.jsonToValue(el) for el in jsonData]
+            
+        return jsonData
+    
+    @classmethod
+    def fromJson(classobj, obj):
+        print("obj: %s" % obj)
+        assert isinstance(obj, list)
+        assert obj[0] == classobj.__name__
+        data = obj[1]
+        instance = classobj()
+        for k in data:
+            v = classobj.jsonToValue(data[k])
+            instance.__dict__[k] = v
+        return instance
+
 class PersistentObject:
     savepath = ""
     def save(self):
-        pickle.dump(self, open(self.savepath, "wb+"))
+        assert isinstance(self, JsonSerializable)
+        json.dump(self.toJson(), open(self.savepath, "w+"))
 
     @classmethod
     def load(classObj):
+        assert issubclass(classObj, JsonSerializable)
         data = classObj()
         if os.path.isfile(classObj.savepath):
-            data = pickle.load(open(classObj.savepath, "rb"))
+            jsondata = json.load(open(classObj.savepath, "r+"))
+            data = classObj.fromJson(jsondata)
         return data
+
+            
 
 ###############################################################################
 ## Players
 
-class Player:
-    def __init__(self, name):
-        self.name = name
+class Player(JsonSerializable):
+    def __init__(self, playerName = ""):
+        self.name   = playerName
+        self.id     = str(uuid.uuid3(uuid.NAMESPACE_URL, playerName))
 
-class PlayersDB(PersistentObject):
+class PlayersDB(PersistentObject, JsonSerializable):
     savepath = 'players.dat'
     
     def __init__(self):
         self.players = []
 
-    def add(self, newPlayer):
-        alreadyThere = list(filter(lambda x: x.name == newPlayer.name, self.players)) != []
-        if not alreadyThere:
-            self.players += [newPlayer]
-            return True
+    def add(self, newPlayerName):
+        playerData = [p for p in self.players if p.name == newPlayerName]
+        playerFound = playerData != []
+        if playerFound:
+            return playerData
         else:
-            return False
+            newPlayer = Player(newPlayerName)
+            self.players += [newPlayer]
+            return newPlayer
+    
+    def get(self, name):
+        for p in self.players:
+            if p.name == name:
+                return p
+        return False
 
 @app.route('/player', methods=['POST', 'GET'])
 def player():
@@ -82,36 +139,60 @@ def player():
     players = PlayersDB.load()
     if cmd == 'add':
         name = request.args.get('player','')
-        # colors = request.args.get('colors','')
-        newPlayer = Player(name)
-        if players.add(newPlayer):
-            players.save()
-            return "player %s added" % name
-        else:
-            return "player already present..."
+        players.add(name)
+        players.save()
+        return "player %s added" % name
     return 'unknown command'
 
 ###############################################################################
 ## Seasons
 
-class Season():
-    def __init__(self, db, setname):
+class Season(JsonSerializable):
+    class PlayerInfo(JsonSerializable):
+        def __init__(self, id = ""):
+            self.playerId = id
+            self.deckColors = []
+            self.paid = False
+
+    def __init__(self, db = None, setname = ""):
         self.set = setname
         self.registeredPlayers = []
         self.matches = []
+        self.rarePool = []
         self.db = db
     
     def generateMatches(self):
         self.match = [] # todo
 
-    def registerPlayer(name):
-        if name not in self.registerPlayer:
-            players = PlayersDB.load()
-            players.add(Player(name))
-            registerPlayer += [name]
+    def registerPlayer(self, name):
+        players = PlayersDB.load()
+        playerData = players.add(name)
+        isRegistered = next((p for p in self.registeredPlayers if playerData.id == p.playerId), False)
+        if not isRegistered:
+            self.registeredPlayers += [Season.PlayerInfo(playerData.id)]
             db.save()
 
-class SeasonsDB(PersistentObject):
+    def addToRaresPool(self, cardIds):
+        for newRareId in cardIds:
+            added = False
+            for rare in self.rarePool:
+                if rare['id'] == newRareId:
+                    rare['count'] += 1
+                    added = True
+                    break
+            if not added:
+                self.rarePool += [{'id' : newRareId, 'count' : 1}]
+        db.save()
+
+    def removeRaresFromPool(self, cardIds):
+        for rareId in cardIds:
+            for r in self.rarePool:
+                if r['id'] == rareId:
+                    r['count'] -= 1
+        self.rarePool = [r for r in self.rarePool if r['count'] > 0]
+        db.save()
+    
+class SeasonsDB(PersistentObject, JsonSerializable):
     savepath = 'seasons.dat'
 
     def __init__(self):
@@ -123,6 +204,7 @@ class SeasonsDB(PersistentObject):
                 return False
         self.seasons += [Season(self, setname)]
         self.save()
+        return True
 
     def getSeason(self, setname):
         for s in self.season:
@@ -145,19 +227,27 @@ def season():
     currentSeason = seasons.getLatestSeason()
     if cmd == 'register':
         name = request.args.get('player','')
-        if name != "" and currentSeason:
+        if not currentSeason:
+            return "no seasons started"
+        elif name == "":
+            return "invalid player name"
+        else:
             currentSeason.registerPlayer(name)
             return "registered player: %s" % name
     elif cmd == 'newseason':
         set = request.args.get('set','')
-        if set != "":
+        if set == "":
+            return "invalid set name..."
+        else:
             if seasons.newSeason(set):
                 return "started season: %s" % set
             else:
-                return "season: %s already started" % set
+                return "couldn't start new season %s" % set
     
     return "unknown command"
         
+###############################################################################
+## main
 
 if __name__ == '__main__':
     app.run(debug=True)
