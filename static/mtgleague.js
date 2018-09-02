6bf76b173082eecb97a7db7136ea85ee2e8b1580
David St-Hilaire <sthilaire.david@gmail.com>
2018-09-02 15:08:57 -0400

    start of a dynamic webinterfaced for the seasons page

--- static/mtgleague.js
+++ static/mtgleague.js
@@ -4,30 +4,44 @@ class newSeason
 	{
 		var setnameOption = document.getElementById('setname');
 		var setid = setnameOption.value
-		alert('id: '+setid)
 		if (setnameOption)
 		{
 			var xhttp =  new XMLHttpRequest();
 			xhttp.onreadystatechange = function() {
-				alert("xhttp.status: "+ xhttp.status)
-				if (xhttp.status == 200)
-					newSeason.setStatus("season created")
-				else
-					newSeason.setStatus("failed to create season...")
+				if (xhttp.readyState != 4)
+					return;
+				var color = xhttp.status == 201 ? "green" : "red"
+				newSeason.setStatus(xhttp.responseText, false);
 			};
-			xhttp.open("GET", "season?cmd='new'&set="+setid, true);
+			xhttp.open("GET", "/season-api?cmd=new&set="+setid, true);
 			xhttp.send();
 		}
 	}
 
 	static updateStatus()
 	{
-		alert('updateStatus()')
+		var setnameOption = document.getElementById('setname');
+		var setid = setnameOption.value
+		var xhttp =  new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState != 4)
+				return;
+			var isExisting = xhttp.responseText != "false"
+			var color = isExisting ? "red" : "green"
+			var msg = isExisting ? "Season already started on " + xhttp.responseText + "..." : "Set available"
+			newSeason.setStatus(msg, !isExisting, color)
+		};
+		xhttp.open("GET", "/season-api?cmd=isExisting&set="+setid, true);
+		xhttp.send();		
 	}
 
-	static setStatus(status)
+	static setStatus(status, okToSend, color)
 	{
 		var statusEl = document.getElementById('status');
-		statusEl.value = status
+		statusEl.textContent = status
+		statusEl.style.color = color
+
+		var button = document.getElementById('createButton');
+		button.style.display = okToSend ? 'inline' : 'none'
 	}
 }
